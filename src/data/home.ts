38097da1dd9b1f9fcf019b4ba4ad81cf2b2893ac
fuya <143484868+fuyaram@users.noreply.github.com>
2024-01-06 19:08:41 +0530

    Add files via upload

---
+++ src/data/home.ts
@@ -0,0 +1,264 @@
+export const categories = [
+    "All",
+    "JavaScript",
+    "TypeScript",
+    "Programming",
+    "Travelling",
+    "Cricket World Cup 2023",
+    "Bowling",
+    "Cricket",
+    "React",
+    "Next.js",
+    "UI/UX Design",
+    "Object Oriented Programming",
+    "Frontend Web Development",
+    "Backend Web Development",
+    "Web Development",
+    "Coding",
+]
+
+export const videos = [
+    {
+        id: "1",
+        title: "Spotify Clone Using HTML CSS | Creating a Spotify Clone",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 888000,
+        postedAt: new Date("2023-09-29"),
+        duration: 2586,
+        thumbnailUrl: "https://i3.ytimg.com/vi/TdjmTjuU8JE/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "2",
+        title: "Infinite Scrolling Animation CSS Only",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 257136,
+        postedAt: new Date("2023-11-14"),
+        duration: 732,
+        thumbnailUrl: "https://i3.ytimg.com/vi/0QI4ymWwpG0/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "3",
+        title: "How to get ahead of 99% of people",
+        channel: {
+            name: "Ruri Ohama",
+            id: "Ruri Ohama",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 777777,
+        postedAt: new Date("2023-9-08"),
+        duration: 1117,
+        thumbnailUrl: "https://i3.ytimg.com/vi/p_0MaEUBMYg/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "4",
+        title: "Sia - Unstoppable (Official Audio)",
+        channel: {
+            name: "Sia",
+            id: "Sia",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 172464814,
+        postedAt: new Date("2016-1-21"),
+        duration: 217,
+        thumbnailUrl: "https://i3.ytimg.com/vi/cxjvTXo9WWM/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "5",
+        title: "THE BATMAN – Main Trailer",
+        channel: {
+            name: "Warner Bros. Pictures",
+            id: "Warner Bros. Pictures",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 60000000,
+        postedAt: new Date("2021-11-02"),
+        duration: 1000,
+        thumbnailUrl: "https://i3.ytimg.com/vi/mqqft2x_Aa4/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "6",
+        title: "Marvel Studios' Avengers: Endgame - Official Trailer",
+        channel: {
+            name: "Marvel Entertainment",
+            id: "Marvel Entertainment",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 103400000,
+        postedAt: new Date("2020-03-03"),
+        duration: 126,
+        thumbnailUrl: "https://i3.ytimg.com/vi/TcMBFSGVi1c/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "7",
+        title: "Learn React From Scratch in 1 hour",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 1032330,
+        postedAt: new Date("2023-08-09"),
+        duration: 2781,
+        thumbnailUrl: "https://i3.ytimg.com/vi/c8-eYB--j-Q/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "8",
+        title: "Hidden Details in Loki's Costumes! | Behind the Scenes",
+        channel: {
+            name: "Marvel Entertainment",
+            id: "Marvel Entertainment",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 20323340,
+        postedAt: new Date("2023-11-17"),
+        duration: 307,
+        thumbnailUrl: "https://i3.ytimg.com/vi/mS9nfsA-oGI/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "9",
+        title: "Beautiful Scroll Animations | Animate on Scroll",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 20323,
+        postedAt: new Date("2023-10-17"),
+        duration: 643,
+        thumbnailUrl: "https://i3.ytimg.com/vi/UYSylqXW9vM/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "10",
+        title: "Peaky Blinders Season 6 Official Trailer | Netflix India",
+        channel: {
+            name: "Netflix India",
+            id: "Netflix India",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 5323,
+        postedAt: new Date("2023-8-17"),
+        duration: 93,
+        thumbnailUrl: "https://i3.ytimg.com/vi/PxZ5gGfPtCQ/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+
+    {
+        id: "11",
+        title: "Setup Tailwind CSS with React JS | Added Tips to use it easily",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 20323,
+        postedAt: new Date("2023-9-21"),
+        duration: 256,
+        thumbnailUrl: "https://i3.ytimg.com/vi/JtEZjrfx3oM/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "12",
+        title: "Learn useState Hook in React JS | React Hooks Explained",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 30323,
+        postedAt: new Date("2023-9-17"),
+        duration: 990,
+        thumbnailUrl: "https://i3.ytimg.com/vi/z_MnNejwR8s/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "13",
+        title: "Carousel Component in ReactJS and Tailwind CSS",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 10323,
+        postedAt: new Date("2023-8-17"),
+        duration: 1347,
+        thumbnailUrl: "https://i3.ytimg.com/vi/i1lbfhKmD6c/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "14",
+        title: "Website With Animated Text Effect Using HTML And CSS",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 5323,
+        postedAt: new Date("2023-8-17"),
+        duration: 1113,
+        thumbnailUrl: "https://i3.ytimg.com/vi/SdiYKfjg53g/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+        id: "15",
+        title: "Magic Navigation Menu Indicator Using HTML CSS JavaScript",
+        channel: {
+            name: "fuya",
+            id: "constGenius",
+            profileUrl:
+                "https://avatars.githubusercontent.com/u/143484868?v=4",
+        },
+        views: 8323,
+        postedAt: new Date("2023-11-15"),
+        duration: 1113,
+        thumbnailUrl: "https://i3.ytimg.com/vi/ZCpQm7Qn03k/maxresdefault.jpg",
+        videoUrl:
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+
+
+]
